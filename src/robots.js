82a444d8c9c3c35478912362abb7cb64666a6b50
Boris <dragon_fury@abv.bg>
2018-10-04 16:25:04 +0300

    second init

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Stephanie Todorova',
+        username: 'Stephanie', 
+        email: 'Stef@leaf.com'
+    },
+    {
+        id: 2,
+        name: 'Jermen Todorova',
+        username: 'ju', 
+        email: 'Ju@zen.org'
+    },
+    {
+        id: 3,
+        name: 'Pavel Shtiliyanov',
+        username: 'Pavel', 
+        email: 'Pavel@volleyball.tv'
+    },
+    {
+        id: 4,
+        name: 'Mihail Milkov',
+        username: 'Mihail', 
+        email: 'Mihail@bike.biz'
+    },
+    {
+        id: 5,
+        name: 'Boris Shtiliyanov',
+        username: 'Boris', 
+        email: 'Boris@comp.net'
+    },
+]
